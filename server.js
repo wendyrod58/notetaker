123017e6b939597dcb1874edebb699f7e54a6d3a
Wendy Rodriguez <wendyrod58@gmail.com>
2022-03-14 18:01:41 -0700

    set up server file

---
+++ server.js
@@ -0,0 +1,17 @@
+//require express 
+const express = require('express'); 
+
+//initialize the server 
+const app = express(); 
+
+//setup PORT number 
+const PORT = process.env.PORT || 3001; 
+
+//setup jSON and parsing for the server 
+
+//public folder accessible 
+
+
+//setup the diff routes 
+
+// listen to the server 
